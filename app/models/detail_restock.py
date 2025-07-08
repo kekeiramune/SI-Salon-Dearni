a342338f9c16e50ae8f3fe9f33147254fc38e6a0
abiyumuflih8 <abiyumuflih8@email.com>
2025-07-08 22:01:04 +0700

    update detail restock

--- app/models/detail_restock.py
+++ app/models/detail_restock.py
@@ -1,23 +1,23 @@
 from app.config.Database import mysql
 
-def get_detail_by_restock(id_detail_restock):
+def get_detail_by_restock(id_restock):
     cursor = mysql.connection.cursor()
     cursor.execute("""
         SELECT dr.*, p.nama_produk 
         FROM detail_restock dr
         JOIN produk p ON dr.id_produk = p.id_produk
-        WHERE id_restock=%s
-    """, (id_detail_restock,))
+        WHERE dr.id_restock=%s
+    """, (id_restock,))
     result = cursor.fetchall()
     cursor.close()
     return result
 
-def insert_detail(id_detail_restock, id_produk, jumlah, harga_satuan):
+def insert_detail(id_restock, id_produk, jumlah, harga_satuan):
     cursor = mysql.connection.cursor()
     cursor.execute("""
         INSERT INTO detail_restock (id_restock, id_produk, jumlah, harga_satuan)
         VALUES (%s, %s, %s, %s)
-    """, (id_detail_restock, id_produk, jumlah, harga_satuan))
+    """, (id_restock, id_produk, jumlah, harga_satuan))
     mysql.connection.commit()
     cursor.close()
     return True
